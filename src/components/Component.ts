6e952c0ec4f6db5b671e7c00654f818bc830bfaf
Choo King <choocodes@gmail.com>
2024-06-17 18:51:59 -0400

    Give the Component class a default object for holding children.

--- src/components/Component.ts
+++ src/components/Component.ts
@@ -1,6 +1,7 @@
 export abstract class Component<T extends HTMLElement> {
     element: T;
     parent: HTMLDivElement;
+    children = {} as {[key: string]: HTMLElement|Component<any>;};
     protected constructor(parent: HTMLDivElement, elementType:string = "div") {
         this.element = document.createElement(elementType) as T;
         this.element.classList.add("hidden");
