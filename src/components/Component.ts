a651c47caa1d5123b7a6012b538d26171077c8b3
Choo King <choocodes@gmail.com>
2024-06-19 15:25:33 -0400

    Add option in constructor to decide if component is hidden initially.

--- src/components/Component.ts
+++ src/components/Component.ts
@@ -1,10 +1,11 @@
+export type Children<T extends Component<any>> = {[key: string]: T;}
 export abstract class Component<T extends HTMLElement> {
     element: T;
     parent: HTMLDivElement;
-    children = {} as {[key: string]: HTMLElement|Component<any>;};
-    protected constructor(parent: HTMLDivElement, elementType:string = "div") {
+    //children = {} as {[key: string]: HTMLElement|Component<any>;};
+    protected constructor(parent: HTMLDivElement, elementType:string = "div", hidden = false) {
         this.element = document.createElement(elementType) as T;
-        this.element.classList.add("hidden");
+        if(hidden) this.element.classList.add("hidden");
         this.parent = parent;
     }
     abstract render(target: HTMLDivElement):void;
