f698cde9cbb382d36297ef5b8667f448ec996a83
Choo King <choocodes@gmail.com>
2024-06-19 14:32:38 -0400

    Make constructor protected

--- src/components/Input/input.ts
+++ src/components/Input/input.ts
@@ -3,7 +3,7 @@ import {Component} from "../Component.ts";
 export abstract class Input<T> extends Component<HTMLInputElement>{
     _value: T | null = null;
     callback: (value: T) => void;
-    constructor(parent: HTMLDivElement, callback: (value: T) => void) {
+    protected constructor(parent: HTMLDivElement, callback: (value: T) => void) {
         super(parent, "input");
         this.callback = callback;
         this.render(parent);
