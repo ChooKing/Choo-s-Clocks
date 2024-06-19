475f3cd929aee62a2d8d9f364b4e1764fa009057
Choo King <choocodes@gmail.com>
2024-06-19 15:26:54 -0400

    Implement new Button class as wrapper of HTML button.

---
+++ src/components/button/button.ts
@@ -0,0 +1,21 @@
+import {Component} from "../Component.ts";
+
+export class Button extends Component<HTMLButtonElement>{
+    _text: string;
+    constructor(parent: HTMLDivElement, text: string, hidden: boolean = false) {
+        super(parent, "button", hidden);
+        this._text = text;
+        this.render(parent);
+    }
+    render(target: HTMLDivElement): void {
+        this.element.innerText = this._text;
+        target.appendChild(this.element);
+    }
+    setHandler(callback: () => void){
+        this.element.addEventListener("click", callback);
+    }
+    update(value: string){
+        this._text = value;
+    }
+
+}
