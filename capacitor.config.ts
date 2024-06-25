404060bc85f6043d30a63f386a136b87a810db06
Choo King <choocodes@gmail.com>
2024-06-25 17:13:46 -0400

    Enable notifications.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'king.choo.app',
   appName: 'choo-clocks',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "./assets/public/ring.mp3",
+    },
+  },
 };
 
 export default config;
