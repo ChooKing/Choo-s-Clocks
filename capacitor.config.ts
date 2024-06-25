59515e12857b0a331ac4bdc67036b1145bee1c24
Choo King <choocodes@gmail.com>
2024-06-25 15:50:15 -0400

    Add Capacitor and set up Android project.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'king.choo.app',
+  appName: 'choo-clocks',
+  webDir: 'dist'
+};
+
+export default config;
